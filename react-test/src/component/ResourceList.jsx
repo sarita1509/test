9d482dda7eb8d7fb8344ff394b2d6260a36356fb
Sarita <131792757+sarita1509@users.noreply.github.com>
2024-08-09 08:32:32 +0000

    "commit"

--- react-test/src/component/ResourceList.jsx
+++ react-test/src/component/ResourceList.jsx
@@ -1,26 +1,23 @@
-import React from "react";
+import React from 'react';
 
-const ResourceList = ({resource}) => {
-    return(
-        <div>
-            <h2>Resource</h2>
-            <ul>
-                {resource.map((resource,index) => (
-                    <li key={index}>
-                        <p><strong>Date of registertion:</strong> {resource.dateOfRegistertaion}</p>
-                        
-                        <p><strong>vender Score:</strong> {resource.venderScore}</p>
-                        <p><strong>status:</strong> {resource.status}</p>
-                        <p><strong>Type of Buiness:</strong> {resource.typeOfBuiness}</p>
-                        <p><strong>Location:</strong> {resource.location}</p>
-                        <p><strong>assign to:</strong> {resource.assignTo}</p>
-                        
-                    
-                    </li>
-                ))}
-            </ul>
-
-        </div>
-    );
+const ResourceList = ({ resources }) => {
+  return (
+    <div>
+      <h2>Resources</h2>
+      <ul>
+        {resources.map((resource, index) => (
+          <li key={index}>
+            <p><strong>Date of Register:</strong> {resource.dateOfRegister}</p>
+            <p><strong>Vendor Score:</strong> {resource.vendorScore}</p>
+            <p><strong>Status:</strong> {resource.status}</p>
+            <p><strong>Type of Business:</strong> {resource.businessType}</p>
+            <p><strong>Location:</strong> {resource.location}</p>
+            <p><strong>Assign To:</strong> {resource.assignTo}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
 export default ResourceList;
